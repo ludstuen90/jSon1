885fd44dece04079780beded9c09db239de2d4d1
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-06-05 14:49:48 -0500

    Added city, shoutout, and CSS styling

--- scripts/app.js
+++ scripts/app.js
@@ -40,17 +40,36 @@ $(document).ready(function(){
     var lastName = friends.students[i].last_name;
     var lastNameC = (lastName.charAt(0).toUpperCase() + lastName.slice(1));
     var city = friends.students[i].city;
+    var cityC = (city.charAt(0).toUpperCase() + city.slice(1));
     var shoutOut = friends.students[i].shoutout;
+        //clear and replace existing div
         $('#classMate').remove();
         var showMe= document.createElement('div');
         showMe.id= "classMate";
+        // generate First name from JSON
         var firstNameContent = document.createElement('p');
         firstNameContent.textContent = "First Name: " + firstNameC;
         showMe.appendChild(firstNameContent);
+        // generate Last name from JSON
+
         var lastNameContent = document.createElement('p');
         lastNameContent.textContent = "Last Name: " + lastNameC;
         showMe.appendChild(lastNameContent);
+        // generate city from JSON
+        var cityContent = document.createElement('p');
+        cityContent.textContent = "City: " + cityC;
+        showMe.appendChild(cityContent);
+
+          //generate shoutout from JSON
+          var soContent = document.createElement('p');
+          soContent.textContent = shoutOut;
+          showMe.appendChild(soContent);
+
+
+
+        //update Progress Indicator
         var progress = document.createElement('p');
+
         var bumpUp = (i+1);
         progress.textContent = bumpUp + " / 20";
         showMe.appendChild(progress);
