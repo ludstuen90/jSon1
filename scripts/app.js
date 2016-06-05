88265da696ec228717d1a79b9b0998dc603a4aad
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-06-05 14:41:40 -0500

    Progress bar fully working

--- scripts/app.js
+++ scripts/app.js
@@ -11,21 +11,14 @@ $(document).ready(function(){
     success: function (data) {
       console.log('in ajax success');
       friends= JSON.parse(data);
-      var firstName = friends.students[i].first_name;
-      var lastName = friends.students[i].last_name;
-      var city = friends.students[i].city;
-      var shoutOut = friends.students[i].shoutout;
 
-      console.log("The coolest student is " + firstName + " "  + lastName + ",  who lives in " + city + " and I'd like to say! " + shoutOut);
+      studentGenerator();
 
 
-      $('#classMate').remove();
-      var showMe= document.createElement('div');
-      showMe.id= "classMate";
-      var paragraph = document.createElement('p');
-      paragraph.textContent = "First Name: " + friends.students[1].first_name;
-      showMe.appendChild(paragraph);
-      $('body').append(showMe);
+
+
+//Empty contents of the '#classMate' DIV on the page, and write the student's information for index [i]
+//of the students array
 
     },
     statusCode: {
@@ -40,8 +33,52 @@ $(document).ready(function(){
 
 
 
+  var studentGenerator = function() {
 
+    var firstName = friends.students[i].first_name;
+    var firstNameC = (firstName.charAt(0).toUpperCase() + firstName.slice(1));
+    var lastName = friends.students[i].last_name;
+    var lastNameC = (lastName.charAt(0).toUpperCase() + lastName.slice(1));
+    var city = friends.students[i].city;
+    var shoutOut = friends.students[i].shoutout;
+        $('#classMate').remove();
+        var showMe= document.createElement('div');
+        showMe.id= "classMate";
+        var firstNameContent = document.createElement('p');
+        firstNameContent.textContent = "First Name: " + firstNameC;
+        showMe.appendChild(firstNameContent);
+        var lastNameContent = document.createElement('p');
+        lastNameContent.textContent = "Last Name: " + lastNameC;
+        showMe.appendChild(lastNameContent);
+        var progress = document.createElement('p');
+        var bumpUp = (i+1);
+        progress.textContent = bumpUp + " / 20";
+        showMe.appendChild(progress);
+        $('body').append(showMe);
+};
 
+var nextStudent = $(document).on('click', '#nextS', function () {
+  if( friends.students[(i +1)] === undefined) {
+    alert("Sorry! You're already at the last student.");
+  }
 
+else {
+  i++;
+  console.log(i);
+
+  studentGenerator();
+}
+});
+
+var prevStudent =$(document).on('click', '#prevS', function() {
+  if(i===0) {
+    alert("You're already at the first student.");
+  }
+  else {
+    i--;
+    console.log(i);
+    studentGenerator();
+  }
+});
 
 });
